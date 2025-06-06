55e73213c803d8d2dce39ae1fc42e539e673d784
GIRISH RANJAN <166251518+Girish2819@users.noreply.github.com>
2025-06-06 23:21:29 +0530

    admin middleware added

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,11 @@
 import JWT from "jsonwebtoken";
 import {registerController} from '../controllers/authController.js';
-
+import userModel from "../models/userModel.js";
 //protecting routes token base 
 export const requireSignIn=async (req, res, next) => {
    try{
-     const decode =JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+     const decode =JWT.verify(req.headers.authorization, process.env.JWT_SECRET);
+     req.user = decode; // attach user info to request object
     next();
    }catch (error) {
        console.log(error);
@@ -15,4 +16,27 @@ export const requireSignIn=async (req, res, next) => {
        });
    }
 
-}
+};
+
+// admin access
+export const isAdmin = async (req, res, next) => {
+    try{
+        const user =await userModel.findById(req.user._id)
+        if (user.role !== 1) {
+            return res.status(401).send({
+                success: false,
+                message: 'UnAuthorized Access'
+            });
+        } else{
+            next();
+        }
+    }catch (error) {
+        console.log(error);
+        res.status(401).send({
+            success: false,
+             error,
+            message: 'Error in Admin Middleware',
+           
+        });
+    }
+};
