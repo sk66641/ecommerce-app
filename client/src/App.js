3991de74d7bb86d931f3712920f5b9ccbf58bb0a
GIRISH RANJAN <166251518+Girish2819@users.noreply.github.com>
2025-06-07 19:50:22 +0530

    pages are added, components are created and routes are created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 
-import './App.css';
+import { Routes,Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Policy from './pages/Policy';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <>
-    <h1>Ecommerce-App</h1>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/About" element={<About/>} />
+      <Route path="/Contact" element={<Contact/>} />
+      <Route path="/Policy" element={<Policy />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+   
     </>
   );
 }
