21de6a52f41effe76c9d50b1b811530168af93d7
GIRISH RANJAN <166251518+Girish2819@users.noreply.github.com>
2025-06-08 14:09:56 +0530

    contact page updated

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import { BiMailSend, BiPhoneCall, BiSupport } from 'react-icons/bi' // ✅ Import these icons
 
 const Contact = () => {
   return (
     <Layout>
-        <h1>Contact Page</h1>
+      <div className="row contacts">
+        <div className="col-md-6">
+          <img 
+            src="/images/contacts.jpg"
+            alt="contacts"
+            style={{ width: '100%' }}
+          />
+        </div>
+        <div className="col-md-4"> 
+          <h1 className="bg-dark text-white text-center">CONTACT US</h1> 
+          <p className="text-justify mt-2">
+            Any query and info about product, feel free to call anytime. We are available 24x7.
+          </p>
+          <p className="mt-3">
+            <BiMailSend /> : www.help@ecommerceapp.components
+          </p>
+          <p className="mt-3">
+            <BiPhoneCall /> : +91 1234567890
+          </p>
+          <p className="mt-3">
+            <BiSupport /> : 1800-123-4567 (toll free)
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import Layout from '../components/layout/Layout'
 
 const PageNotFound = () => {
   return (
     <Layout>
-        <h1>PageNotFound</h1>
+        <div className='pnf'>
+          <h1 className='pnf-title'>404</h1>
+          <h2 className='pnf-heading'>Oops ! Page Not Found</h2>
+          < Link to='/' classname='pnf-btn'>
+          <button>Go Back</button>
+          </Link>
+        </div>
     </Layout>
   )
 }
 
-export default PageNotFound
+export default PageNotFound 
