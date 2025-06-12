6f0d2f1a733e6e5ccba1422ba029b1a3c4fea8f3
Girish Ranjan <girishranjan71@gmail.com>
2025-06-12 23:52:52 +0530

    line 17 to 30 has to be recheck

--- client/src/context/Auth.js
+++ client/src/context/Auth.js
@@ -10,14 +10,24 @@ const [auth, setAuth] = useState({
     user: null,
     token: "",
     });
-// useEffect(() => {
-//     const storedAuth = localStorage.getItem("auth");
-//     if (storedAuth) {
-//         setAuth(JSON.parse(storedAuth));
-//     }
-// }, [auth]);
-//there is some error in the above code, it should not depend on auth, it should be empty array
+useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
 
+    if (storedUser && storedToken) {
+        setAuth({
+            user: JSON.parse(storedUser),
+            token: storedToken,
+        });
+    }
+}
+, [auth]);    
+// Save auth state to localStorage whenever it changes
+useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(auth.user));
+    localStorage.setItem("token", auth.token);
+}
+, [auth]);      
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
         {children}
