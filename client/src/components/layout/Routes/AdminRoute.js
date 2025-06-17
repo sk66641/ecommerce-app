4d1c20dd3994202c9f0aaca41759854afe0a7670
Girish Ranjan <girishranjan71@gmail.com>
2025-06-18 04:50:17 +0530

    admin route created

---
+++ client/src/components/layout/Routes/AdminRoute.js
@@ -0,0 +1,48 @@
+import { useEffect,useState } from "react"; 
+import { useAuth } from "../../../context/Auth";
+import { Outlet } from "react-router-dom";
+import axios from "axios";
+import Spinners from "../Spinners"; // Importing a spinner component for loading state
+
+
+
+
+export default function AdminRoute(){
+    const [ok, setOk] = useState(false);
+    const {auth, setAuth} = useAuth()
+    useEffect(() => {
+        const authcheck = async () => {
+        try {
+    const res = await axios.get('/api/v1/auth/admin-auth', {
+      headers: {
+        Authorization: auth?.token
+      }
+    });
+        if (res.data.ok) {
+            setOk(true);
+            setAuth({
+            ...auth,
+            user: res.data.user,
+            token: auth?.token // keep the same token
+      });
+      localStorage.setItem("auth", JSON.stringify({
+        ...auth,
+        user: res.data.user
+      }));
+    } else {
+      setOk(false);
+ }
+    } catch (err) {
+    setOk(false);
+    console.log("Auth check error", err);
+  }
+};
+        if (auth?.token) {
+    authcheck();
+  } else {
+    setOk(false); // If no token, redirect to login via spinner
+  }
+    }, [auth ?.token]);
+    return ok ? <Outlet/> :<Spinners/>;
+
+}
---
+++ client/src/pages/Admin/AdminDashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Layout from '../../components/layout/Layout'
+import { useAuth } from "../../context/Auth";
+const AdminDashboard = () => {
+  const [auth] = useAuth();
+  return (
+    <Layout>
+          <div className="col-md-9">
+            <div className="card w-75 p-3">
+              <h3> Admin Name : {auth?.user?.name}</h3>
+              <h3> Admin Email : {auth?.user?.email}</h3>
+              <h3> Admin Contact : {auth?.user?.phone}</h3>
+            </div>
+          </div>
+    </Layout>
+  );
+};
+
+export default AdminDashboard;
